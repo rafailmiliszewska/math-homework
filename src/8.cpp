85ee8158492678484411d3150154d7daa63e9e75
rafailmiliszewska <rafailmiliszewska@gmail.com>
2025-03-14 18:47:40 +0000

    Add 8.cpp

---
+++ src/8.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include <cmath>
+
+int main() {
+  int num = 10;
+  float result = sqrt(num);
+  std::cout << "The square root of " << num << " is: " << result << std::endl;
+  return 0;
+}
