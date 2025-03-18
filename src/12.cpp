0908eb78952569f1d4bb29d4148add71a5f08bc0
rafailmiliszewska <rafailmiliszewska@gmail.com>
2025-03-18 02:48:01 +0000

    Add 12.cpp

---
+++ src/12.cpp
@@ -0,0 +1,10 @@
+
+  // write a function that takes two integers as input and returns their sum
+  int sum(int x, int y) {
+    return x + y;
+  }
+
+  int main() {
+    cout << sum(5, 7) << endl; // should print 12
+    return 0;
+  }
