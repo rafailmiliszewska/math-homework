00f34000abb6689b76434182a81c8faf1f3bd056
rafailmiliszewska <rafailmiliszewska@gmail.com>
2025-03-30 17:52:50 +0000

    Add 20.cpp

---
+++ src/20.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cout << "Enter an integer: ";
+    cin >> n;
+    if (n % 2 == 0) {
+        cout << "The number is even.\n";
+    } else {
+        cout << "The number is odd.\n";
+    }
+}
