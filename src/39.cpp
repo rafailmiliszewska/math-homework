54464d360d84b6c2c1ed680fbf85ce3346d8b8da
rafailmiliszewska <rafailmiliszewska@gmail.com>
2025-05-18 17:14:55 +0000

    Add 39.cpp

---
+++ src/39.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a = 5;
+    int b = 3;
+    int c = 7;
+    
+    cout << "The sum of " << a << " and " << b << " is " << (a + b) << endl;
+    cout << "The product of " << c << " and " << b << " is " << (c * b) << endl;
+
+    return 0;
+}
